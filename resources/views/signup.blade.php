ae11a5593d0c955b28064f3d35c8704666190214
David Dieperink <david.dieperink@ik.me>
2022-12-08 21:04:49 +0100

    created a sign up form

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,5 +1,10 @@
 @extends('layout')
 
 @section('content')
-
+    <form method="post">
+        <input type="email" placeholder="Email">
+        <input type="password" placeholder="Password">
+        <input type="password" placeholder="Password (confirmation)">
+        <input type="submit" value="Sign Up">
+    </form>
 @endsection
