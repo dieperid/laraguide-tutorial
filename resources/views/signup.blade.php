ceaf48f28a1bbea9bb64b683fb895315c8de5906
David Dieperink <david.dieperink@ik.me>
2022-12-08 21:08:35 +0100

    added csrf_field to secure the form

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <form method="post">
+        {{csrf_field()}}
         <input type="email" placeholder="Email">
         <input type="password" placeholder="Password">
         <input type="password" placeholder="Password (confirmation)">
