f2489200dfc98faf9fcce4b2a537a7cbf86a7b00
David Dieperink <david.dieperink@ik.me>
2022-12-08 21:09:35 +0100

    added required parameter to the form input

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <form method="post">
         {{csrf_field()}}
-        <input type="email" placeholder="Email">
-        <input type="password" placeholder="Password">
-        <input type="password" placeholder="Password (confirmation)">
+        <input type="email" placeholder="Email" required>
+        <input type="password" placeholder="Password" required>
+        <input type="password" placeholder="Password (confirmation)" required>
         <input type="submit" value="Sign Up">
     </form>
 @endsection
