12cf4a65203bb651d80108996c349e82af8c5e6a
David Dieperink <david.dieperink@ik.me>
2022-12-12 18:31:17 +0100

    added email and password parameters to the up function

--- database/migrations/2022_12_12_172853_create_user_table.php
+++ database/migrations/2022_12_12_172853_create_user_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
